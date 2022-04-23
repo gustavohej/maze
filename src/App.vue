a3c08c8d96a363006d36a1bd98d37398c1bb68c9
Gustavo Henrique <gustavohej@gmail.com>
2022-04-23 17:08:23 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Maze from "./components/Maze.vue";
+</script>
+
+<template>
+  <Maze />
+</template>
+
+<style>
+body {
+  background-color: #282c34;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #7bd18e;
+  margin-top: 60px;
+}
+</style>
